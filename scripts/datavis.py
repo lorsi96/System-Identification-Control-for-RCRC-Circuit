9ea2f15ff875c220cbd20a0dc3505c9b614f2311
Lucas Orsi <lorsi@skyloom.co>
2023-03-07 23:48:16 -0300

    DAC & ADC Working with 10Hz Pulse

---
+++ scripts/datavis.py
@@ -0,0 +1,178 @@
+#!python3
+import numpy as np
+import csv
+import time
+import matplotlib.pyplot as plt
+import functools
+from   matplotlib.animation import FuncAnimation
+import io
+from typing import Mapping, Any, Callable, BinaryIO, Dict, List, Tuple, Protocol
+import serial
+
+STREAM_FILE=("/dev/ttyUSB1", "serial")
+OUTPUTFILEPATH='out/output.csv'
+HeaderSpec = Mapping[str, Callable[[BinaryIO], int]]
+Header = Mapping[str, Any]
+SerialData = Tuple[Header, List[float]]
+
+# ********************************* Parsers ********************************* #
+def stdint_read(f:BinaryIO, size_bytes=2, signed=False) -> int:
+    raw = bytes()
+    for _ in range(size_bytes):
+        raw += f.read(1)
+    return int.from_bytes(raw, "little", signed=signed)
+
+# ********************************* Streams ********************************* #
+class Streamable(Protocol):
+    def open(self) -> BinaryIO:
+        ...
+    
+    def flush(self):
+        ...
+
+class SerialStreamable(Streamable):
+    def __init__(self, port:str='/dev/ttyUSB1') -> None:
+        self.port = port
+    
+    def open(self) -> BinaryIO:
+        self.f = serial.Serial(port=self.port, baudrate=460800, timeout=None)
+        return self.f  # type: ignore
+    
+    def flush(self):
+        self.f.flushInput()
+
+class FileStreamable(Streamable):
+    def __init__(self, sz_bytes, filepath:str='serial') -> None:
+        self.fpath = filepath
+        self.sz = sz_bytes
+
+    def open(self) -> BinaryIO:
+        self.f = open(self.fpath, "rb", 0)
+        self.flush()
+        return self.f
+    
+    def flush(self):
+        self.f.seek(2*self.sz, io.SEEK_END)
+
+# ****************************** Serial Manager ***************************** #
+class SerialHeaderManager:
+    HEAD = b'head'
+    TAIL = b'tail'
+    def __init__(self, file:BinaryIO, spec: HeaderSpec, def_packet:Header) -> None:
+        self._file = file 
+        self._spec = spec 
+        self._last_packet = def_packet
+    
+    def wait_for_packet(self) -> SerialData:
+        return (
+            self.__find_header(), 
+            self.__read_data(self._last_packet['N']))
+    
+    def __find_header(self) -> Mapping[str, Any]:
+        found = False 
+        ret: Dict[str, Any] = {"head": SerialHeaderManager.HEAD}
+        while not found:
+            self.__find_head()
+            for key, parser in self._spec.items():
+                ret.update({key: parser(self._file)})
+            found = self.__find_tail()
+        self._last_packet = ret
+        return ret
+    
+    def __read_data(self, n:int) -> List[float]:
+        parse = lambda: stdint_read(self._file, signed=True) / 0xFFFF * 1.65
+        return [parse() for _ in range(n)]
+
+    def __find_head(self):
+        data=bytearray(len(SerialHeaderManager.HEAD))
+        while data!=SerialHeaderManager.HEAD:
+            raw=self._file.read(1)
+            data+=raw
+            data[:]=data[-4:]
+
+    def __find_tail(self):
+        data=bytearray(b'1234')
+        for _ in range(4):
+            data+=self._file.read(1)
+            data[:]=data[-4:]
+        return data == SerialHeaderManager.TAIL
+
+# ******************************** Data Dump ******************************** #
+
+
+# ********************************* Plotting ******************************** #
+fig = plt.figure(1)
+
+
+adcAxe = fig.add_subplot ( 2,1,1                  )
+adcLn, = plt.plot        ( [],[],'r-',linewidth=4 )
+adcAxe.grid              ( True                   )
+adcAxe.set_ylim          ( -1.65 ,1.65            )
+adcAxe.set_title('Scope')
+adcAxe.set_xlabel('Time [Sec]')
+adcAxe.set_ylabel('Mag')
+
+fftAxe = fig.add_subplot ( 2,1,2                  )
+fftLn, = plt.plot        ( [],[],'b-',linewidth=4 )
+fftAxe.grid              ( True                   )
+# fftAxe.set_ylim          ( 0 ,0.25                )
+fftAxe.set_ylim          ( -0.05 ,0.05            )
+fftAxe.set_title('Dbg Signal')
+fftAxe.set_xlabel('Time [Sec]')
+fftAxe.set_ylabel('Mag')
+
+plt.tight_layout()
+
+header = {
+    "head": b"head", 
+    "id": 0, 
+    "N":512, 
+    "fs": 8000, 
+    "tail":b"tail"
+}
+
+header_spec = {
+    "id": functools.partial(stdint_read, size_bytes=4), 
+    "N": stdint_read, 
+    "fs": stdint_read, 
+}
+
+
+outfile = open(OUTPUTFILEPATH, 'w')
+csv_writer = csv.DictWriter(outfile, fieldnames=('Data',))
+csv_writer.writeheader()
+stream = SerialStreamable()
+stream_file = stream.open()
+serial_manager = SerialHeaderManager(stream_file, header_spec, header)
+
+rec=np.ndarray(1).astype(np.int16)
+
+def init():
+    global rec
+    rec=np.ndarray(1).astype(np.int16)
+    return adcLn, fftLn
+
+def update(t):
+    global header,rec, outfile
+    found_h, raw_data  = serial_manager.wait_for_packet() 
+    id, N, fs = found_h["id"], found_h["N"], found_h["fs"]
+    
+    adc   = np.array(raw_data)
+    time  = np.arange(0, N/fs, 1/fs)
+    csv_writer.writerows(map(lambda v: {'Data': v}, adc))
+
+    adcAxe.set_xlim ( 0    ,N/fs )
+    adcLn.set_data  ( time ,adc  )
+
+    rec=np.concatenate((rec,((adc/1.65)*2**(15-1)).astype(np.int16)))
+
+    return adcLn, fftLn
+
+
+if __name__ == '__main__':
+    ani=FuncAnimation(fig, update, 128, init_func=init, blit=True, interval=1, 
+                    repeat=True)
+    plt.draw()
+    plt.show()
+    stream_file.close()
+    outfile.close()
--- src/main.c
+++ src/main.c
@@ -4,14 +4,38 @@
 #include "arm_const_structs.h"
 #include "arm_math.h"
 #include "sapi.h"
+
 /* ************************************************************************* */
 /*                               Configuration                               */
 /* ************************************************************************* */
-#define PROG_LOOP_HZ 8000
+#define PROG_LOOP_HZ 1000
+#define SQ_WAVE_HZ 10
 #define PROG_FREQ_CYCLES (EDU_CIAA_NXP_CLOCK_SPEED / PROG_LOOP_HZ)
 
 #define UART_BAUDRATE 460800
 
+
+typedef int16_t(*DacSampleProvider_t)();
+
+/* ************************************************************************* */
+typedef struct {
+    uint32_t ticksThreshold; 
+    uint32_t ticksThresholdHalf;
+    uint32_t tickCount;
+} SquareWaveDacProvider_t;
+
+int16_t SquareWaveDacProvider_getSample() {
+    static SquareWaveDacProvider_t self = {
+        .ticksThreshold = PROG_LOOP_HZ / SQ_WAVE_HZ, 
+        .ticksThresholdHalf = PROG_LOOP_HZ / SQ_WAVE_HZ / 2,
+        .tickCount = 0
+    };
+
+    if(self.tickCount >= self.ticksThreshold) {
+        self.tickCount = 0;
+    }
+    return self.tickCount++ > self.ticksThresholdHalf ? 0x7FFF : 0x8000;
+}
 
 #define ADC_DATA_SZ 512
 /* ***************************** Data Transfer ***************************** */
@@ -20,12 +44,9 @@ static struct header_struct {
     uint32_t id;
     uint16_t N;
     uint16_t fs;
-    uint16_t dbg1;
-    uint16_t dbg2;
-    uint16_t dbg3;
     char tail[4];
 } header = {
-    "head", 0, ADC_DATA_SZ, PROG_LOOP_HZ, 0, 0, 0, "tail"};
+    "head", 0, ADC_DATA_SZ, PROG_LOOP_HZ, "tail"};
 
 q15_t adc_sample = 0;
 uint16_t dac_sample = 512;
@@ -33,26 +54,36 @@ uint16_t dac_sample = 512;
 /* ************************************************************************* */
 /*                                    Code                                   */
 /* ************************************************************************* */
+static DacSampleProvider_t dacProvider = SquareWaveDacProvider_getSample; 
+
 
 int main(void) {
+
+
+
     boardConfig();
     uartConfig(UART_USB, UART_BAUDRATE);
     adcConfig(ADC_ENABLE);
     dacConfig(DAC_ENABLE);
     cyclesCounterInit(EDU_CIAA_NXP_CLOCK_SPEED);
     dacWrite(DAC, 512);  // Start at 0.
+    static uint32_t uartSamplesCount = 0;
     for (;;) {
         cyclesCounterReset();
 
         /* Send ADC data through UART. */
-        uint16_t raw_adc = adcRead(CH1);
+        uint16_t raw_adc = adcRead(CH2);
         adc_sample = (((raw_adc - 512)) << 6);
         uartWriteByteArray(UART_USB, (uint8_t*)&adc_sample, sizeof(adc_sample));
-
-        // uartWriteByteArray(UART_USB, (uint8_t*)&header, sizeof(header));
         // uartReadByte(UART_USB, &recv_by);
 
-        // dacWrite(DAC, (modulator_get_out_sample(&mod) >> 6) + 512);
+        dacWrite(DAC, (dacProvider() >> 6) + 512);
+
+        if(++uartSamplesCount == ADC_DATA_SZ) {
+            uartWriteByteArray(UART_USB, (uint8_t*)&header, sizeof(header));
+            uartSamplesCount = 0;
+        }
+
         while (cyclesCounterRead() < PROG_FREQ_CYCLES)
             ;
     }
